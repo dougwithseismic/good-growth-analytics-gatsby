59a1d14ac78d6895cc5fece2ffd23174cacfe71a
Doug <doug@withseismic.com>
2020-05-12 13:51:08 +0200

    typekit addition

--- src/components/contentGrid/gridItem/index.js
+++ src/components/contentGrid/gridItem/index.js
@@ -7,8 +7,8 @@ const GridItem = ({ content: { subtitle, title, cta, readTime } }) => {
   return (
     <Link className="grid-item" to={`/article-template`} style={{ textDecoration: 'none' }}>
       <div className="grid-text">
-        <div className="grid-subtitle">{subtitle}</div>
-        <div className="grid-title">{title}</div>
+        <h3 className="grid-subtitle">{subtitle}</h3>
+        <h1 className="grid-title">{title}</h1>
       </div>
       <div className="grid-details">
         <div className="grid-read-time">{readTime}</div>

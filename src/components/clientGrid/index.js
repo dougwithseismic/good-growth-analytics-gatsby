382a9369160f25bdb4d5645947b73fbbeedbdcc7
Doug <doug@withseismic.com>
2020-05-14 20:59:42 +0200

    ClientGrid

---
+++ src/components/clientGrid/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Img from 'gatsby-image'
+import { graphql, useStaticQuery } from 'gatsby'
+
+import './clientGrid.scss'
+
+const ClientGrid = (props) => {
+  const data = useStaticQuery(graphql`
+    {
+      allFile(filter: { relativeDirectory: { eq: "clients" } }) {
+        nodes {
+          id
+          childImageSharp {
+            fluid(maxWidth: 250, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp_noBase64
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <section className="client-grid">
+      <div className="container">
+        <h3>CLIENTS</h3>
+        <div className="client-logo-grid">
+          {data.allFile.nodes.map((image) => (
+            <Img className="client-logo" fluid={image.childImageSharp.fluid} alt="Client Logos" />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ClientGrid
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from './../components/layout'
 import NewsletterSignup from './../components/newsletter/index'
 
 import './home.scss'
+import ClientGrid from './../components/clientGrid/index'
 
 const IndexPage = (props) => {
   return (
@@ -13,14 +14,13 @@ const IndexPage = (props) => {
       <Hero />
       {/* <div className=" jumbo container"> <span>Latest Content</span> </div> */}
       <ContentGrid />
+      <ClientGrid />
       <NewsletterSignup>
         {({ onSignup }) => (
           <section className="newsletter-signup">
             <div className="container">
               <div className="newsletter-content">
-                <h2>
-                  This is a header too, but not your regular spiel.
-                </h2>
+                <h2>This is a header too, but not your regular spiel.</h2>
                 <p>
                   But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
                   and I will give you a complete account of the system, and expound the actual teachings of the great

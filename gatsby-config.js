6780f4c5d71affb4bd56eee95931a9a00b7bad8d
Doug <doug@withseismic.com>
2020-05-17 22:24:42 +0200

    GTM fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        id: 'GTM-111111',
+        id: 'GTM-NC9K7NM',
 
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
